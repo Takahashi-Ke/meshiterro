5e581b9045428e31e62442c35dff9c52af6dd02d
EC2 Default User <ec2-user@ip-172-31-72-97.ec2.internal>
2020-10-20 00:02:46 +0000

    [Add]いいね機能のモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   #親のresourcesに子のresourcesを指定することを「ネストする」という
   #URLがコメント投稿と投稿画像で関連づけられるためユーザにとってわかりやすい
   resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
