8e9fbc1b9ab68545c6ca738b3e92b3da978b36af
EC2 Default User <ec2-user@ip-172-31-72-97.ec2.internal>
2020-10-20 03:13:37 +0000

    [Start]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   #親のresourcesに子のresourcesを指定することを「ネストする」という
   #URLがコメント投稿と投稿画像で関連づけられるためユーザにとってわかりやすい
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
